9a7e2d34ec8b68dfb90fd407abc7fc9177bd6309
AdityaSinghNPST <aditya.singh@npsts.com>
2026-08-18 12:50:07 +0530

    Initilize Spring boot Project

---
+++ src/main/java/com/library/Aditya_Lib_Fine_Calc/AdityaLibFineCalcApplication.java
@@ -0,0 +1,13 @@
+package com.library.Aditya_Lib_Fine_Calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdityaLibFineCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdityaLibFineCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/Aditya_Lib_Fine_Calc/AdityaLibFineCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.Aditya_Lib_Fine_Calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdityaLibFineCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
